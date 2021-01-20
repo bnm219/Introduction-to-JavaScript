9a8240196609d848cbff9e5ab82e71624a9e6928
Bri <brittanymorris@MacBook-Pro.local>
2021-01-19 21:50:52 -0700

    I am pushing because my empty push did not work.

--- index.js
+++ index.js
@@ -258,6 +258,7 @@ function annoyingSong(number){
 
 //Grade Calculator
 /*
+
 Using the grade function below do the following: 
   1. Receive a score out of 100 
   2. Return the corresponding letter grade following this grade scale:
